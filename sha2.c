f0c08b85873829870420885eedb9933801447bec
djb <digitalbitbox@gmail.com>
2015-05-17 18:24:34 +0200

    comment out unused variable

--- sha2.c
+++ sha2.c
@@ -257,7 +257,7 @@ static const sha2_word64 K512[80] = {
 };
 
 /* Initial hash value H for SHA-384 */
-static const sha2_word64 sha384_initial_hash_value[8] = {
+/*static const sha2_word64 sha384_initial_hash_value[8] = {
 	0xcbbb9d5dc1059ed8ULL,
 	0x629a292a367cd507ULL,
 	0x9159015a3070dd17ULL,
@@ -266,7 +266,7 @@ static const sha2_word64 sha384_initial_hash_value[8] = {
 	0x8eb44a8768581511ULL,
 	0xdb0c2e0d64f98fa7ULL,
 	0x47b5481dbefa4fa4ULL
-};
+};*/
 
 /* Initial hash value H for SHA-512 */
 static const sha2_word64 sha512_initial_hash_value[8] = {

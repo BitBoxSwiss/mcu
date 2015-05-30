86eb17027d1e4d4580adb4b3d7a9f3a4ef3a7cf1
djb <digitalbitbox@gmail.com>
2015-05-30 17:32:56 +0200

    fix valgrind error; remove sign speed test

--- tests_api.c
+++ tests_api.c
@@ -177,52 +177,6 @@ static int tests_report_has(const char *str)
 }
 
 
-static void tests_sign_speed(void)
-{
-	// N = 50 -> 7.5 sig/sec on chip
-	uint8_t sig[64], priv_key_0[32], priv_key_1[32], msg[256];
-	uint16_t time_ms;
-	size_t i, N = 50; 
-	int res;
-	for (i = 0; i < sizeof(msg); i++) {
-		msg[i] = i * 1103515245;
-	}
-
-	memcpy(priv_key_0, utils_hex_to_uint8("c55ece858b0ddd5263f96810fe14437cd3b5e1fbd7c6a2ec1e031f05e86d8bd5"), 32);
-	memcpy(priv_key_1, utils_hex_to_uint8("509a0382ff5da48e402967a671bdcde70046d07f0df52cff12e8e3883b426a0a"), 32);
-
-#ifdef TESTING
-    clock_t t = clock();
-#else
-	NVIC_SetPriority(SysTick_IRQn, 0); // Make high priority so that we can timeout
-	systick_current_time_ms = 0;
-#endif
-	
-	for (i = 0 ; i < N; i++) {
-		res = uECC_sign(priv_key_0, msg, sizeof(msg), sig);
-	}
-
-	for (i = 0 ; i < N; i++) {
-		res += uECC_sign(priv_key_1, msg, sizeof(msg), sig);
-	}
-
-#ifdef TESTING
-	time_ms = (float)(clock() - t) * 1000 / CLOCKS_PER_SEC;
-#else
-    time_ms = systick_current_time_ms;
-	NVIC_SetPriority(SysTick_IRQn, 15); // Reset lower priority
-#endif
-	
-    if (res) {
-		tests_fill_report("tests_sign_speed", "FAIL - Could not sign");
-	} else {
-		char report[64];
-		sprintf(report, "%0.2f sig/s", N * 2.0 / ((float)time_ms / 1000));
-		tests_fill_report("tests_sign_speed", report);
-	}
-}
-
-
 static void tests_seed_xpub_backup(void)
 {
     char xpub0[112], xpub1[112];
@@ -705,6 +659,8 @@ static void tests_aes_cbc(void)
     char encrypt[] = "{\"type\":\"encrypt\", \"data\":\"";
     char decrypt[] = "{\"type\":\"decrypt\", \"data\":\"";
     char enc[COMMANDER_REPORT_SIZE * 2], dec[COMMANDER_REPORT_SIZE * 2];
+    memset(enc, 0, sizeof(enc));
+    memset(dec, 0, sizeof(dec));
 
 	static const char *aes_vector[] = {
 		// plain                               cipher
@@ -789,16 +745,15 @@ static void tests_aes_cbc(void)
 
 static void tests_run(void)
 {
-    tests_seed_xpub_backup();
+    tests_aes_cbc();
     tests_sign();
     tests_name();
     tests_password();
     tests_device();
     tests_random();
     tests_input();
-    tests_aes_cbc();
     tests_verifypass();
-    tests_sign_speed();	
+    tests_seed_xpub_backup();
 }
 
 

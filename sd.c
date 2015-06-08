919b38116c7044f53319e6f53a6118d25cf1c2b2
djb <digitalbitbox@gmail.com>
2015-06-09 00:01:33 +0200

    upload hardware interface functions

---
+++ sd.c
@@ -0,0 +1,283 @@
+/*
+ 
+ The MIT License (MIT)
+ 
+ Copyright (c) 2015 Douglas J. Bakkum
+
+ Permission is hereby granted, free of charge, to any person obtaining
+ a copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included
+ in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+
+#include <string.h>
+#include <stdio.h>
+#include "sd.h"
+#include "commander.h"
+#include "flags.h"
+#include "mcu.h"
+
+uint32_t sd_update = 0;
+uint32_t sd_fs_found = 0;
+uint32_t sd_listing_pos = 0;
+uint32_t sd_num_files = 0;
+
+FATFS fs;
+
+
+uint8_t sd_write(const char *f, int f_len, const char *t, int t_len)
+{
+	char file[256] = {0};
+	memcpy(file, "0:", 2);
+	memcpy(file+2, f, (f_len < 256 - 2) ? f_len : 256 - 2);
+	
+    char text[256] = {0};
+	if (t_len > 256) {
+		commander_fill_report("sd_write", FLAG_ERR_SD_WRITE_LEN, ERROR);
+		return ERROR;
+	}
+	
+    memcpy(text, t, t_len);
+    sd_mmc_init();
+	sd_listing_pos = 0;
+
+	if (CTRL_FAIL == sd_mmc_test_unit_ready(0)) {
+		commander_fill_report("sd_write", FLAG_ERR_SD_CARD, ERROR);
+	} else {
+		FRESULT res;
+		FIL file_object;
+
+		memset(&fs, 0, sizeof(FATFS));
+		res = f_mount(LUN_ID_SD_MMC_0_MEM, &fs);
+		if (FR_INVALID_DRIVE == res) {
+		    commander_fill_report("sd_write", FLAG_ERR_SD_MOUNT, ERROR);
+			return ERROR;
+		}
+
+		file[0] = LUN_ID_SD_MMC_0_MEM + '0';
+		res = f_open(&file_object, (char const *)file, FA_CREATE_NEW | FA_WRITE);
+		if (res != FR_OK) {
+		    commander_fill_report("sd_write", FLAG_ERR_SD_FILE_EXISTS, ERROR);
+			return ERROR;
+		}
+
+		if (0 == f_puts(text, &file_object)) {
+			f_close(&file_object);
+		    commander_fill_report("sd_write", FLAG_ERR_SD_WRITE, ERROR);
+			return ERROR;
+		}
+		
+        f_close(&file_object);
+		commander_fill_report("sd_write", "success", SUCCESS);
+		
+		f_mount(LUN_ID_SD_MMC_0_MEM, NULL); 
+	}
+	return SUCCESS;
+}
+
+
+char *sd_load(const char *f, int f_len)
+{
+	char file[256] = {0};
+	memcpy(file, "0:", 2);
+	memcpy(file + 2, f, (f_len < 256 - 2) ? f_len : 256 - 2);
+
+	static char text[256];
+	memset(text, 0, sizeof(text));
+
+    sd_mmc_init();
+	sd_listing_pos = 0;
+
+	if (CTRL_FAIL == sd_mmc_test_unit_ready(0)) {
+		commander_fill_report("sd_load", FLAG_ERR_SD_CARD, ERROR);
+		return NULL;
+	}
+
+	FRESULT res;
+	FIL file_object;
+	memset(&fs, 0, sizeof(FATFS));
+	res = f_mount(LUN_ID_SD_MMC_0_MEM, &fs);
+	if (FR_INVALID_DRIVE == res) {
+		commander_fill_report("sd_load", FLAG_ERR_SD_MOUNT, ERROR);
+		return NULL;
+	}
+
+	file[0] = LUN_ID_SD_MMC_0_MEM + '0';
+	res = f_open(&file_object, (char const *)file, FA_OPEN_EXISTING | FA_READ);
+	if (res != FR_OK) {
+		commander_fill_report("sd_load", FLAG_ERR_SD_OPEN, ERROR);
+		return NULL;
+	}
+
+	if (0 == f_gets(text, sizeof(text), &file_object)) {
+		f_close(&file_object);
+		commander_fill_report("sd_load", FLAG_ERR_SD_READ, ERROR);
+		return NULL;
+	}
+		
+	f_close(&file_object);
+	commander_fill_report("sd_load", "success", SUCCESS);
+		
+	f_mount(LUN_ID_SD_MMC_0_MEM, NULL);
+	return text;
+}
+
+
+uint8_t sd_list(void)
+{
+	FILINFO fno;
+	DIR dir;
+	char *pc_fn;
+	const char *path = "0:";
+	#if _USE_LFN
+	char c_lfn[_MAX_LFN + 1];
+	fno.lfname = c_lfn;
+	fno.lfsize = sizeof(c_lfn);
+	#endif
+
+	char files[1028] = {0};
+	uint32_t pos = 1;
+	
+	
+	sd_mmc_init();
+	sd_listing_pos = 0;
+
+	if (CTRL_FAIL == sd_mmc_test_unit_ready(0)) {
+		commander_fill_report("sd_list", FLAG_ERR_SD_CARD, ERROR);
+		return ERROR;
+	}
+
+	FRESULT res;
+	memset(&fs, 0, sizeof(FATFS));
+	res = f_mount(LUN_ID_SD_MMC_0_MEM, &fs);
+	if (FR_INVALID_DRIVE == res) {
+		commander_fill_report("sd_list", FLAG_ERR_SD_MOUNT, ERROR);
+		return ERROR;
+	}
+
+	// Open the directory
+	res = f_opendir(&dir, path);
+	if (res == FR_OK)
+	{
+		for (;;)
+		{
+			res = f_readdir(&dir, &fno);
+			if (res != FR_OK || fno.fname[0] == 0)
+			{
+				break;
+			}
+
+			#if _USE_LFN
+			pc_fn = *fno.lfname ? fno.lfname : fno.fname;
+			#else
+			pc_fn = fno.fname;
+			#endif
+			if (*pc_fn == '.')
+			{
+				continue;
+			}
+
+			if (pos >= sd_listing_pos)
+			{
+				strcat(files, "/");
+				strcat(files, pc_fn);
+				strcat(files, ", ");
+			}
+
+			pos += 1;
+		}
+	}
+	
+	// Unmount
+	f_mount(LUN_ID_SD_MMC_0_MEM, NULL);
+
+	commander_fill_report("sd_list", files, SUCCESS);
+	return SUCCESS;
+}
+
+
+
+uint8_t sd_erase(void)
+{
+	int erased = 0;
+	FILINFO fno;
+	DIR dir;
+	char *pc_fn;
+	const char *path = "0:";
+	#if _USE_LFN
+	char c_lfn[_MAX_LFN + 1];
+	fno.lfname = c_lfn;
+	fno.lfsize = sizeof(c_lfn);
+	#endif
+
+	sd_mmc_init();
+	sd_listing_pos = 0;
+
+	if (CTRL_FAIL == sd_mmc_test_unit_ready(0)) {
+		commander_fill_report("sd_erase", FLAG_ERR_SD_CARD, ERROR);
+		return ERROR;
+	}
+
+	FRESULT res;
+	memset(&fs, 0, sizeof(FATFS));
+	res = f_mount(LUN_ID_SD_MMC_0_MEM, &fs);
+	if (FR_INVALID_DRIVE == res) {
+		commander_fill_report("sd_erase", FLAG_ERR_SD_MOUNT, ERROR);
+		return ERROR;
+	}
+	
+	// Open the directory
+	res = f_opendir(&dir, path);
+	if (res == FR_OK)
+	{
+		for (;;)
+		{
+			res = f_readdir(&dir, &fno);
+			if (res != FR_OK || fno.fname[0] == 0)
+			{
+				break;
+			}
+
+			#if _USE_LFN
+			pc_fn = *fno.lfname ? fno.lfname : fno.fname;
+			#else
+			pc_fn = fno.fname;
+			#endif
+			if (*pc_fn == '.')
+			{
+				continue;
+			}
+
+			if(f_unlink(pc_fn) == FR_OK) {
+				erased++;
+			}
+			
+		}
+	} 
+	
+	// Unmount
+	f_mount(LUN_ID_SD_MMC_0_MEM, NULL);
+
+	if(erased) {
+		commander_fill_report("sd_erase", "success", SUCCESS);
+		return SUCCESS;
+	} else {
+		commander_fill_report("sd_erase", FLAG_ERR_SD_NO_FILE, ERROR);
+		return ERROR;
+	}
+}
---
+++ touch.h
@@ -0,0 +1,53 @@
+/*
+
+ The MIT License (MIT)
+
+ Copyright (c) 2015 Douglas J. Bakkum
+
+ Permission is hereby granted, free of charge, to any person obtaining
+ a copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included
+ in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+
+#ifndef _TOUCH_H_
+#define _TOUCH_H_
+
+#include <stdint.h>
+
+#ifndef TESTING
+#define TOUCH_CHANNEL					CHANNEL_9
+#define QTOUCH_LIB_TYPE_MASK			0x01
+#define QTOUCH_LIB_COMPILER_OFFSET		2
+#define QTOUCH_LIB_COMPILER_MASK		0x01
+#define QTOUCH_LIB_MAX_CHANNEL_OFFSET   3
+#define QTOUCH_LIB_MAX_CHANNEL_MASK		0x7F
+#define QTOUCH_LIB_KEY_ONLY_OFFSET		10
+#define QTOUCH_LIB_KEY_ONLY_MASK		0x01
+#define QTOUCH_LIB_ROTOR_NUM_OFFSET		11
+#define QTOUCH_LIB_ROTOR_NUM_MASK		0x1F
+#define GET_SENSOR_STATE(SENSOR_NUMBER) (qt_measure_data.qt_touch_status.sensor_states[(SENSOR_NUMBER/8)] & (1 << (SENSOR_NUMBER % 8)))
+#endif
+
+
+void touch_button_parameters(uint16_t timeout, uint16_t threshold);
+void touch_init(void);
+uint8_t touch_button_press(int long_touch);
+
+
+#endif

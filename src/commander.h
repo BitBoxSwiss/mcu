86e45a932bae984c366925bd1ce082b2e5c656e8
djb <digitalbitbox@gmail.com>
2015-09-09 22:36:20 +0200

    increase password attempts before auto erase

--- src/commander.h
+++ src/commander.h
@@ -43,7 +43,7 @@
 
 #define COMMANDER_ARRAY_ELEMENT_MAX  1024
 #define VERIFYPASS_FILENAME         "verification.txt"
-#define COMMANDER_MAX_ATTEMPTS      5// max attempts before device reset
+#define COMMANDER_MAX_ATTEMPTS      15// max attempts before device reset
 
 #define AUTOBACKUP_FILENAME         "autobackup_"
 #define AUTOBACKUP_ENCRYPT          "yes"

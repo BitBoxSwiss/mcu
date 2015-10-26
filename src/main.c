136357852aef34e804231e86d2e76327a439e6bf
djb <digitalbitbox@gmail.com>
2015-10-26 10:12:49 +0100

    add low level mcu driver files

---
+++ src/main.c
@@ -0,0 +1,75 @@
+/*
+
+ The MIT License (MIT)
+
+ Copyright (c) 2015 Douglas J. Bakkum
+
+ Permission is hereby granted, free of charge, to any person obtaining
+ a copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included
+ in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+
+#include "drivers/config/conf_usb.h"
+#include "drivers/config/mcu.h"
+#include "sd.h"
+#include "usb.h"
+#include "ecc.h"
+#include "led.h"
+#include "touch.h"
+#include "memory.h"
+#include "systick.h"
+#include "ataes132.h"
+#include "commander.h"
+
+
+void SysTick_Handler(void)
+{
+    systick_update_time();
+}
+
+
+int main (void)
+{
+    wdt_disable(WDT);
+    irq_initialize_vectors();
+    cpu_irq_enable();
+    sleepmgr_init();
+    sysclk_init();
+    board_init();
+    aes_init();
+    flash_init(FLASH_ACCESS_MODE_128, 6);
+    pmc_enable_periph_clk(ID_PIOA); // Button input
+    usb_suspend_action();
+    udc_start();
+    delay_init(F_CPU);
+    memory_setup(); // One time factory setup
+    systick_init();
+    touch_init();
+    ecc_context_init();
+
+    led_off();
+    delay_ms(300);
+    led_on();
+    delay_ms(300);
+    led_off();
+
+    while (true) {
+        sleepmgr_enter_sleep();
+    }
+}

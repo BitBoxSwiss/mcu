6c7bedf922d440cbfcee74bcabf27c92b976e474
djb <djb@shiftdevices.com>
2016-06-06 12:02:32 +0200

    minor fix to pdf format

--- src/sd.h
+++ src/sd.h
@@ -42,7 +42,7 @@
 #define SD_PDF_BACKUP_END "<2020202020 20> Tj"
 #define SD_PDF_TEXT_START "BT\n/F1 12 Tf\n50 700 Td\n(Encrypted wallet backup:) Tj\n" SD_PDF_BACKUP_START "\n0 -40 Td\n("
 #define SD_PDF_TEXT_CONTINUE ") Tj\n0 -16 Td\n("
-#define SD_PDF_TEXT_END ") Tj\n" SD_PDF_BACKUP_END "\n0 -60 Td\n(Password:  ______________________) Tj\nET\n/F1 10 Tf\n0 -60 Td\n(Wallet format: BIP32 master extended private key (key path 'm/')) Tj\n0 -14 Td\n(Encryption algorithm: AES-256-CBC) Tj\n0 -14 Td\n(Password hardening: PBKDF2) Tj\n0 -24 Td\n(digitalbitbox.com/backup) Tj\n"
+#define SD_PDF_TEXT_END ") Tj\n" SD_PDF_BACKUP_END "\n0 -60 Td\n(Password:  ______________________) Tj\n/F1 10 Tf\n0 -60 Td\n(Wallet format: BIP32 master extended private key (key path 'm/')) Tj\n0 -14 Td\n(Encryption algorithm: AES-256-CBC) Tj\n0 -14 Td\n(Password hardening: PBKDF2) Tj\n0 -24 Td\n(digitalbitbox.com/backup) Tj\nET\n"
 #define SD_PDF_4_0_END "\nendstream\nendobj\n"
 #define SD_PDF_END  "xref\n0 5\n0000000000 65535 f \n%010i 00000 n \n%010i 00000 n \n%010i 00000 n \n%010i 00000 n \ntrailer\n<<\n/Size 5\n/Root 1 0 R\n>>\nstartxref\n%i\n%%%%%%%%EOF"
 
